4e4200e4e9efa73c0dda4d2064ffea727f5fd1d5
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-08-09 17:36:58 +0000

    Improved components

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <footer className="bg-dark text-light text-center text-lg-start py-3">
+        <footer className="bg-dark text-light text-center text-lg-start py-3 bottom-0">
         <div className="text-center p-3">
             Copyright © Your Website 2024
         </div>
--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -5,12 +5,12 @@ export default function Main({children}){
     return(
         <main className="main container flex-column">
             <section className="container-fluid bg-light">
-                <h1 className="my-3">A Warm Welcome</h1>
-                <p className="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore quae blanditiis iste numquam, totam similique quos vitae facilis ipsam? Nesciunt repudiandae ad atque perferendis aut, accusantium numquam ratione tenetur.</p>
-                <button className="btn btn-primary my-3 mb-5">Call to action!</button>
+                <h1 className="py-1">A Warm Welcome</h1>
+                <p className="py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod tempore quae blanditiis iste numquam, totam similique quos vitae facilis ipsam? Nesciunt repudiandae ad atque perferendis aut, accusantium numquam ratione tenetur.</p>
+                <button className="btn btn-primary my-1 mb-5">Call to action!</button>
             </section>
             <section className="container text-center">
-                <div className="row p-0">
+                <div className="row p-0 my-5">
                     <div className="col pr-0">
                         <Card/>
                     </div>
